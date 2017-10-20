1e8c2f79361329305ff6959ea979ff9ae69e49e2
Nadehh <32816721+Nadehh@users.noreply.github.com>
2017-10-21 00:36:46 +0100

    Updated my Discord Bot to become a simple weather bot.
    The bot returns simple results from using my Python wrapper for the Open weather api

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import discord
 import asyncio
-import os
-import shelve
+import requests
+from weatherUpdate import WeatherUpdate
 
 client = discord.Client()
+weather = WeatherUpdate("API KEY")
+
 
 @client.event
 async def on_ready():
@@ -23,5 +25,27 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    chatMessage = message.content[1:].split()
+
     if message.content[0]=="!":
-        await client.send_message(message.channel,"hi there mate this is a robot")
+
+        if chatMessage[0]=="hello":
+            await client.send_message(message.channel,"hello! I am weather bot :) type !commands for my functionality!")
+
+
+        elif chatMessage[0] == "commands":
+
+            await client.send_message(message.channel,"Command1: temperature\nCommand2: weather\nCommand3: weatherdetail")
+
+
+        elif chatMessage[0]=="weather":
+
+            await client.send_message(message.channel,chatMessage[1]+": "+weather.getWeatherStatus(chatMessage[1]))
+
+        elif chatMessage[0]=="weatherdetail":
+
+            await client.send_message(message.channel,chatMessage[1]+": "+weather.getWeatherStatusDetail(chatMessage[1]))
+
+        elif chatMessage[0]=="temperature":
+            await client.send_message(message.channel,chatMessage[1]+": "+weather.getTemperature(chatMessage[1]+" degrees Celcius"))
